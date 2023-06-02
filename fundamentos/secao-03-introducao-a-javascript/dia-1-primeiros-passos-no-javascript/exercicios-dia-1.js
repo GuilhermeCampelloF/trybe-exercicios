bc0b23bd1b56936fdf372d2026ab7c83906ca250
Guilherme Campello Frizzera <guilhermecampello@hotmail.com>
2023-06-02 11:42:37 -0300

    Adiciona exercício 4

--- fundamentos/secao-03-introducao-a-javascript/dia-1-primeiros-passos-no-javascript/exercicios-dia-1.js
+++ fundamentos/secao-03-introducao-a-javascript/dia-1-primeiros-passos-no-javascript/exercicios-dia-1.js
@@ -1,5 +1,6 @@
 // Exercício 1
-// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para:
+// Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. 
+// É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para:
 
 // Adição (num1 + num2)
 // Subtração (num1 - num2)
@@ -52,3 +53,21 @@ if (number1 > number2 && number1 > number3) {
 } else {
     console.log('Existem números iguais na operação')
 } ;
+
+// Exercício 4
+// Utilize if...else para escrever um código que defina três variáveis com os valores dos três ângulos internos de um triângulo. 
+// Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+
+let angle1 = 30;
+let angle2 = 90;
+let angle3 = 60;
+
+let triangle = angle1 + angle2 + angle3;
+
+if (angle1 <= 0 || angle2 <= 0 || angle3 <= 0) {
+    console.log('Ângulo inválido');
+} else if (triangle === 180) {
+    console.log('true');
+} else if (triangle != 180) {
+    console.log('false');
+}
